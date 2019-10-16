f5922cddf4073c7b16902d10bc559d4e16e1f2e8
mohammadR <tommyrabetian@gmail.com>
2019-10-16 17:44:56 +0330

    seperated parking

--- reservation/models.py
+++ reservation/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from parking_building import ParkingSlot
 
 
 class Reservation(models.Model):
     start_date = models.DateField()
     finish_date = models.DateField()
-    parking_space_number = models.CharField(max_length=4)
+    parking_slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=16)
     name = models.CharField(max_length=35)
     surname = models.CharField(max_length=35)
